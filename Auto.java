e02adb0fdcfdc4f678658eb409334c8a54c5b3d4
Frittenfisch <n.kolde@gmx.net>
2013-02-17 03:31:02 +0100

    blablablk

---
+++ Auto.java
@@ -0,0 +1,45 @@
+
+/**
+ * Write a description of class auto here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Auto
+{
+    // instance variables - replace the example below with your own
+    private String farbe;
+    private final int mörp = 5;
+
+    /**
+     * Constructor for objects of class auto
+     */
+    public Auto(String farb)
+    {
+        // initialise instance variables
+        
+        farbe = farb;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public void sampleMethod(int y)
+    {
+        // put your code here
+     
+       
+    }
+    
+    public int getMörp()
+    {
+        return mörp;
+    }
+    public String getFarbe()
+    {
+        return farbe;    
+    }
+}
---
+++ src/beerunlock/BeerUnlock.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beerunlock;
+
+/**
+ *
+ * @author Nils
+ */
+public class BeerUnlock {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
